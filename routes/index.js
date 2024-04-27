5564e91cd8f71a8af1176e79ee38c69b2e5d068e
Vaidas <vaidas.makstutis@gmail.com>
2024-04-27 18:24:18 +0300

    Upload images to the server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
+var multer = require("multer");
+var path = require("path");
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//multer file storage configuration
+let storage = multer.diskStorage({
+  //store the images in the public/images folder
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  //rename the images
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + "." + file.mimetype.split("/")[1]);
+  }
+});
+
+//configuration for file filter
+let fileFilter = (req, file, callback) => {
+  let ext = path.extname(file.originalname);
+  //if the file extension isn't '.png' or '.jpg' return an error page else return true
+  if (ext !== ".png" && ext !== ".jpg") {
+    return callback(new Error("Only png and jpg files are accepted"));
+  } else {
+    return callback(null, true);
+  }
+};
+
+//initialize Multer with the configurations for storage and file filter
+var upload = multer({ storage, fileFilter: fileFilter });
+
+//create a '/' GET route that'll return the index.html file stored in the public/html folder
+router.get("/", function (req, res, next) {
+  //if there are no image filenames in a session, return the normal HTML page
+  if (req.session.imagefiles === undefined) {
+    res.sendFile(path.join(__dirname, "..", "/public/html/index.html"));
+  } else {
+    //if there are image filenames stored in a session, render them in an index.jade file
+    res.render("index", { images: req.session.imagefiles });
+  }
+});
+router.post("/upload", upload.array("images"), function (req, res) {
+  let files = req.files;
+  let imgNames = [];
+
+  //extract the filenames
+  for (i of files) {
+    let index = Object.keys(i).findIndex(function (e) {
+      return e === "filename";
+    });
+    imgNames.push(Object.values(i)[index]);
+  }
+  //store the image filenames in a session
+  req.session.imagefiles = imgNames;
+
+  //redirect the request to the root URL route
+  res.redirect("/");
 });
 
 module.exports = router;
